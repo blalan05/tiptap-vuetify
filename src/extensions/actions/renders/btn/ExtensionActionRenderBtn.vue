44d3928fb2b7be90ca2a05bf7fb04764b3d96249
Blaine Landowski <blalan05@gmail.com>
2020-09-17 15:16:04 -0500

    Add Remove Tabindex Feature

--- src/extensions/actions/renders/btn/ExtensionActionRenderBtn.vue
+++ src/extensions/actions/renders/btn/ExtensionActionRenderBtn.vue
@@ -10,6 +10,7 @@
           'v-btn--active': $props[PROPS.OPTIONS].isActive($props[PROPS.CONTEXT])
         }"
         :dark="$props[PROPS.DARK]"
+        :tabindex="$props[PROPS.REMOVETABINDEX] ? -1 : 0"
         small
         icon
         v-on="on"
@@ -18,9 +19,7 @@
         <component
           :is="isTextIcon ? 'b' : isVuetifyIcon ? 'v-icon' : null"
           class="tiptap-vuetify-editor__btn-icon"
-        >
-          {{ buttonIcon }}
-        </component>
+        >{{ buttonIcon }}</component>
       </v-btn>
     </template>
     <template>{{ tooltipText }}</template>
@@ -28,82 +27,88 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { Component, Prop } from 'vue-property-decorator'
-import { Editor } from 'tiptap'
-import ExtensionActionRenderBtnOptionsInterface from './ExtensionActionRenderBtnOptionsInterface'
-import TextIcon from '~/extensions/nativeExtensions/icons/TextIcon'
-import VuetifyIcon from '~/extensions/nativeExtensions/icons/VuetifyIcon'
-import IconInterface from '~/extensions/nativeExtensions/icons/IconInterface'
-import { VTooltip, VBtn, VIcon } from 'vuetify/lib'
-import ConsoleLogger from '~/logging/ConsoleLogger'
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { Editor } from "tiptap";
+import ExtensionActionRenderBtnOptionsInterface from "./ExtensionActionRenderBtnOptionsInterface";
+import TextIcon from "~/extensions/nativeExtensions/icons/TextIcon";
+import VuetifyIcon from "~/extensions/nativeExtensions/icons/VuetifyIcon";
+import IconInterface from "~/extensions/nativeExtensions/icons/IconInterface";
+import { VTooltip, VBtn, VIcon } from "vuetify/lib";
+import ConsoleLogger from "~/logging/ConsoleLogger";
 
 // TODO можно использовать как миксин, передавать туда сразу пропсы и не нужно будет писать PROPS = PROPS
 export const PROPS = {
-  EDITOR: 'editor' as const,
-  OPTIONS: 'options' as const,
-  CONTEXT: 'context' as const,
-  DARK: 'dark' as const,
-  DISABLED: 'disabled' as const
-}
+  EDITOR: "editor" as const,
+  OPTIONS: "options" as const,
+  CONTEXT: "context" as const,
+  DARK: "dark" as const,
+  DISABLED: "disabled" as const,
+  REMOVETABINDEX: "removeTabindex" as const,
+};
 
 @Component({
-  components: { VTooltip, VBtn, VIcon }
+  components: { VTooltip, VBtn, VIcon },
 })
 export default class ExtensionActionRenderBtn extends Vue {
   @Prop({ type: Boolean, default: false })
-  readonly [PROPS.DISABLED]: boolean
+  readonly [PROPS.DISABLED]: boolean;
 
   @Prop({ type: Object, required: true })
-  readonly [PROPS.EDITOR]: Editor
+  readonly [PROPS.EDITOR]: Editor;
 
   @Prop({ type: Object, required: true })
-  readonly [PROPS.OPTIONS]: ExtensionActionRenderBtnOptionsInterface
+  readonly [PROPS.OPTIONS]: ExtensionActionRenderBtnOptionsInterface;
 
   @Prop({ type: Object, required: true })
-  readonly [PROPS.CONTEXT]: any
+  readonly [PROPS.CONTEXT]: any;
 
   @Prop({ type: Boolean, default: false })
-  readonly [PROPS.DARK]: boolean
+  readonly [PROPS.DARK]: boolean;
 
-  PROPS = PROPS
+  @Prop({ type: Boolean, default: false })
+  readonly [PROPS.REMOVETABINDEX]: boolean;
 
-  get buttonIcon (): IconInterface {
-    const icon = this[PROPS.OPTIONS].icons[this.$tiptapVuetify.iconsGroup]
+  PROPS = PROPS;
+
+  get buttonIcon(): IconInterface {
+    const icon = this[PROPS.OPTIONS].icons[this.$tiptapVuetify.iconsGroup];
 
     if (!icon) {
-      ConsoleLogger.warn('No icon was provided in extension options.')
+      ConsoleLogger.warn("No icon was provided in extension options.");
 
-      return 'No icon'
+      return "No icon";
     }
 
-    return icon
+    return icon;
   }
 
-  get isTextIcon () {
-    return this.buttonIcon instanceof TextIcon
+  get isTextIcon() {
+    return this.buttonIcon instanceof TextIcon;
   }
 
-  get isVuetifyIcon () {
-    return this.buttonIcon instanceof VuetifyIcon
+  get isVuetifyIcon() {
+    return this.buttonIcon instanceof VuetifyIcon;
   }
 
-  get tooltipText () {
-    const source = this.$props[PROPS.OPTIONS].tooltip
+  get tooltipText() {
+    const source = this.$props[PROPS.OPTIONS].tooltip;
 
-    if (typeof source === 'function') {
-      return source(this.$props[PROPS.CONTEXT], this.$props[PROPS.OPTIONS])
+    if (typeof source === "function") {
+      return source(this.$props[PROPS.CONTEXT], this.$props[PROPS.OPTIONS]);
     }
 
-    return source
+    return source;
   }
 }
 </script>
 
 <style lang="stylus">
-.tiptap-vuetify-editor__action-render-btn
-  margin: 2px 6px
+.tiptap-vuetify-editor__action-render-btn {
+  margin: 2px 6px;
+}
 
-.tiptap-vuetify-editor__btn-icon.v-icon.fas
-  font-size: 16px
+.tiptap-vuetify-editor__btn-icon.v-icon.fas {
+  font-size: 16px;
+}
 </style>
